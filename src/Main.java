91c7695f40ce2dce546957d7ab7ce92ef635d320
DimaK128 <dmytro.kolesnyk@auk.edu.ua>
2023-09-09 03:11:43 +0300

    :)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello, I can advise you on some daily activities based on your age. ");
+        System.out.println("Please,write your age?");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        if (n < 6) {
+            System.out.println("You most likely going to kindergarten or staying at home");
+        }
+        if (n >= 7 && n <= 17) {
+            System.out.println("You most likely going to school");
+        }
+        if (n >= 18 && n <= 22) {
+            System.out.println("You are most likely studying at university");
+        }
+        if (n >= 23 && n <=59) {
+            System.out.println("You are most likely working");
+        }
+        if (n >= 60) {
+            System.out.println("You are most likely retired");
+        }
     }
 }
